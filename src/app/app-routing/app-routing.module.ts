dc02d889b44ea6fe7d69350a95b292fa30e22186
anattzlil <anattzlil@gmail.com>
2018-03-08 12:37:45 +0200

    fixing routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  // {path: 'rooms', component: },
+  {path: '', component: LandingPageComponent},
+  {path: 'rooms', component: HomeComponent},
    {path: 'contact', component: ContactComponent}
 ]
 @NgModule({
